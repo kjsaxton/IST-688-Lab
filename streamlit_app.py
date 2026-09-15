260d0806aa1fddf6673e03834ced395e66fcdd1d
kjsaxton <kjsaxton@syr.edu>
2026-09-15 21:14:41 +0000

    added lab 4 to navigation and made it the defualt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 Lab1=st.Page('Lab1.py', title='Lab1')
-Lab2=st.Page('Lab2.py', title='Lab2', default=True)
+Lab2=st.Page('Lab2.py', title='Lab2',)
 Lab3 = st.Page('Lab3.py', title='Lab3')
+Lab4 = st.Page('Lab4.py', title='Lab4', default=True)
 
-pg = st.navigation([Lab1, Lab2, Lab3])
+pg = st.navigation([Lab1, Lab2, Lab3, Lab4])
 pg.run()
