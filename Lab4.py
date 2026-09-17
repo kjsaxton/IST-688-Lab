21af0c51d0b270993a8fa99d478dec480aec5fd0
kjsaxton <kjsaxton@syr.edu>
2026-09-17 23:42:28 +0000

    fix

--- Lab4.py
+++ Lab4.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from pypdf import PdfReader
 
 # A fix for working with ChromaDB on streamlit community cloud
-__import__('psyqlite3')
+__import__('pysqlite3.')
 # create ChromaDB client
 chroma_client = chromadb.PersistentClient(path='./ChromaDB_for_Lab')
 collection = chroma_client.get_or_create_collection('Lab4Collection')
