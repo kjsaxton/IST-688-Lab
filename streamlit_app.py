491a1c0bf932e2fc2bd83cad81b15914e6ff03f2
kjsaxton <kjsaxton@syr.edu>
2026-09-24 18:06:04 +0000

    lab 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit as st
 Lab1=st.Page('Lab1.py', title='Lab1')
 Lab2=st.Page('Lab2.py', title='Lab2',)
 Lab3 = st.Page('Lab3.py', title='Lab3')
-Lab4 = st.Page('Lab4.py', title='Lab4', default=True)
+Lab4 = st.Page('Lab4.py', title='Lab4')
+Lab4 = st.Page('Lab5.py', title='Lab5', default=True)
 
-pg = st.navigation([Lab1, Lab2, Lab3, Lab4])
+
+pg = st.navigation([Lab1, Lab2, Lab3, Lab4, L])
 pg.run()
